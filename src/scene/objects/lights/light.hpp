5e2554712f14835623973eaa55e55ef761eef71f
LeGourmet <92042633+LeGourmet@users.noreply.github.com>
2024-03-10 21:48:16 +0100

    fix point light bug

--- src/scene/objects/lights/light.hpp
+++ src/scene/objects/lights/light.hpp
@@ -16,7 +16,7 @@ namespace M3D
 		{
 		public:
 			// --------------------------------------------- DESTRUCTOR / CONSTRUCTOR ----------------------------------------------
-			Light(LIGHT_TYPE p_type, const Vec3f& p_color, const float p_intensity, const float p_innerConeAngle = -1.f, const float p_outerConeAngle = -1.f)
+			Light(LIGHT_TYPE p_type, const Vec3f& p_color, const float p_intensity, const float p_innerConeAngle = PIf, const float p_outerConeAngle = PIf)
 				: Object(), _type(p_type), _color(p_color), _intensity(p_intensity / 683.f), _cosInnerConeAngle(glm::cos(p_innerConeAngle)), _cosOuterConeAngle(glm::cos(p_outerConeAngle)){
 				_range = ((p_type==LIGHT_TYPE::DIRECTIONAL) ? FLT_MAX : (float)glm::sqrt(256.f*_intensity*glm::max<float>(_color.x,glm::max<float>(_color.y,_color.z))));
 			}
